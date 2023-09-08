c7dcdd2c7ba6c95d9af390ff1fa4f69faedcc9ad
lewinskie254 <142066223+lewinskie254@users.noreply.github.com>
2023-09-08 20:51:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const menu = document.querySelectorAll('.item-img img')
-const changingImageDiv =  document.querySelector('.changing-image')
-const changingImage = document.querySelector('.changing-image img')
-
-const changePhoto = () => {
-menu.forEach(image =>{
-    image.onclick =() => {
-        changingImageDiv.style.display = 'block'; 
-        changingImage.src = image.getAttribute('src');
-    }
-})} 
-
-changePhoto(); 
+/nan
